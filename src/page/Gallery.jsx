413f68cf1c81e3968a5eb7df8d97359049e71f00
danbijyj <danbijyj3@gmail.com>
2026-01-11 22:24:50 +0900

    Update Gallery.jsx

--- src/page/Gallery.jsx
+++ src/page/Gallery.jsx
@@ -7,7 +7,6 @@ import useAxios from '../hooks/useAxios';
 import { Container } from './style';
 
 const Gallery = () => {
-<<<<<<< HEAD
     const [text, setText] = useState('');
     const API_URL = `50946295-7656ba2a3480c1534c0fb12c0`;
     const url = `https://pixabay.com/api/?key=${API_URL}&q=${text}&image_type=photo`;
@@ -33,33 +32,6 @@ const Gallery = () => {
             </div>
         </Container>
     );
-=======
-  const [text, setText] = useState('');
-  const API_URL = `50946295-7656ba2a3480c1534c0fb12c0`;
-  const url = `https://pixabay.com/api/?key=${API_URL}&q=${text}&image_type=photo`;
-  const { state, loading, error } = useAxios(url);
-  const onSearch = (txt) => {
-    setText(txt);
-  };
-  if (loading) {
-    return <div>로딩중...</div>;
-  }
-  if (error) {
-    return <div>에러메세지</div>;
-  }
-  if (!state) {
-    return null;
-  }
-  return (
-    <Container>
-      <GalleryVisual />
-      <div className="inner">
-        <GalleryInput onSearch={onSearch} />
-        <GalleryList data={state.hits} />
-      </div>
-    </Container>
-  );
->>>>>>> 32960a3fa44a68b8984a91952bb7985840ad511f
 };
 
 export default Gallery;
