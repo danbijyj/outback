5dae13d02cc13685d1d9f6f68bad58444deffd00
danbijyj <danbijyj3@gmail.com>
2026-01-11 23:45:16 +0900

    Update PhotoDetail.jsx

--- src/page/PhotoDetail.jsx
+++ src/page/PhotoDetail.jsx
@@ -3,40 +3,44 @@ import data from '../assets/data';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const PhotoDetail = () => {
-  const navigate = useNavigate();
-  const { photoID } = useParams();
-  const thisPhoto = data.find((item) => item.id === Number(photoID));
-  if (!thisPhoto) {
-    return <div>해당 제품을 찾을 수 없습니다.</div>;
-  }
-  const { id, nameeng, namekor, imgurl, desc, price } = thisPhoto;
-  const onGo1 = () => {
-    navigate('/');
-  };
-  const onGo2 = () => {
-    navigate('/photo');
-  };
-  return (
-    <Container>
-      <PhotoDetailStyle>
-        <div className="inner">
-          <article className="con-box">
-            <strong>{nameeng}</strong>
-            <p>
-              <img src={imgurl} alt={namekor} />
-            </p>
-            <h3>{namekor}</h3>
-            <p>{price}원</p>
-            <p className="desc">{desc}</p>
-            <p className="btn">
-              <button onClick={onGo1}>Home</button>
-              <button onClick={onGo2}>List</button>
-            </p>
-          </article>
-        </div>
-      </PhotoDetailStyle>
-    </Container>
-  );
+    const navigate = useNavigate();
+    const { photoID } = useParams();
+    const thisPhoto = data.find((item) => item.id === Number(photoID));
+    if (!thisPhoto) {
+        return <div>해당 제품을 찾을 수 없습니다.</div>;
+    }
+    const { id, nameeng, namekor, imgurl, desc, price } = thisPhoto;
+    const onGo1 = () => {
+        navigate('/');
+    };
+    const onGo2 = () => {
+        navigate('/photo');
+    };
+    const formatPrice = (price) => {
+        return price.toLocaleString();
+    };
+
+    return (
+        <Container>
+            <PhotoDetailStyle>
+                <div className="inner">
+                    <article className="con-box">
+                        <strong>{nameeng}</strong>
+                        <p>
+                            <img src={imgurl} alt={namekor} />
+                        </p>
+                        <h3>{namekor}</h3>
+                        <p>{formatPrice(price)}원</p>
+                        <p className="desc">{desc}</p>
+                        <p className="btn">
+                            <button onClick={onGo1}>Home</button>
+                            <button onClick={onGo2}>List</button>
+                        </p>
+                    </article>
+                </div>
+            </PhotoDetailStyle>
+        </Container>
+    );
 };
 
 export default PhotoDetail;
